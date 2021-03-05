0f69d3daa891aa070e05cef9ed8ae460c51952a7
Margaux Masson-Forsythe <margaux.masson21@gmail.com>
2021-03-04 17:39:48 -0800

    Added data augmentation in Data Loader and ran first test

--- model-development/data_loader.py
+++ model-development/data_loader.py
@@ -21,20 +21,22 @@ class DataLoader:
                  data_extension_type='.tif',
                  bands=['all'],
                  training_data_shape=(100, 100, 18),
-                 shuffle_and_repeat=False,
-                 enable_just_shuffle=True,
-                 enable_just_repeat=False,
+                 augment=False,
+                 random_flip_up_down=True,
+                 random_flip_left_right=True,
+                 flip_left_right=True,
+                 flip_up_down=True,
+                 rot90=True,
+                 transpose=False,
+                 enable_shuffle=False,
                  training_data_shuffle_buffer_size=1000,
-                 data_repeat_count=None,
                  training_data_batch_size=3,
-                 normalization_value=255.0,
                  training_data_type=tf.float32,
                  label_data_type=tf.uint8,
-                 enable_data_prefetch=True,
-                 data_prefetch_size=2,
+                 enable_data_prefetch=False,
+                 data_prefetch_size=tf.data.experimental.AUTOTUNE,
                  num_parallel_calls=2 * multiprocessing.cpu_count(),
                  output_shape=(tf.float32, tf.uint8)):
-        #TODO add data augmentation e.g https://albumentations.ai/docs/examples/tensorflow-example/
 
         self.label_file_path_train = label_file_path_train
         self.labels_file_train = pd.read_csv(self.label_file_path_train)
@@ -46,12 +48,17 @@ class DataLoader:
         self.bands = bands
         self.bucket_name = bucket_name
 
-        self.enable_just_shuffle = enable_just_shuffle
-        self.enable_just_repeat = enable_just_repeat
-        self.data_repeat_count = data_repeat_count
-        self.enable_data_prefetch = enable_data_prefetch
-        self.shuffle_and_repeat = shuffle_and_repeat
+        self.augment = augment
+        self.random_flip_up_down = random_flip_up_down
+        self.random_flip_left_right = random_flip_left_right
+        self.flip_left_right = flip_left_right
+        self.flip_up_down = flip_up_down
+        self.rot90 = rot90
+        self.transpose = transpose
+
+        self.enable_shuffle = enable_shuffle
         self.num_parallel_calls = num_parallel_calls
+        self.enable_data_prefetch = enable_data_prefetch
         self.data_prefetch_size = data_prefetch_size
         self.data_extension_type = data_extension_type
         self.output_shape = output_shape
@@ -63,7 +70,6 @@ class DataLoader:
             else len(self.training_filenames) // training_data_batch_size
 
         self.training_data_batch_size = training_data_batch_size
-        self.normalization_value = normalization_value
         self.training_data_type = training_data_type
         self.label_data_type = label_data_type
 
@@ -72,46 +78,52 @@ class DataLoader:
 
     def build_training_dataset(self):
         self.training_dataset = tf.data.Dataset.from_tensor_slices(self.training_filenames)
-        self.training_dataset = self.training_dataset.map((
-            lambda x: tf.py_function(self.process_path, [x], self.output_shape)),
-            num_parallel_calls=self.num_parallel_calls)
-        # if self.augment is True:
-        # self.training_dataset = self.training_dataset.map(
-        #         lambda image, label: (tf.image.random_flip_left_right(image), label)
-        #   )
 
-        if self.enable_just_shuffle is True:
+        if self.augment is True:
+            print("Data augmentation enabled ")
+            self.training_dataset_non_aug = self.training_dataset.map((
+                lambda x: tf.py_function(self.process_path, [x], self.output_shape)),
+                num_parallel_calls=self.num_parallel_calls)
+            self.training_dataset_aug = self.training_dataset.map((
+                lambda x: tf.py_function(self.process_path_train_set_augment, [x], self.output_shape)),
+                num_parallel_calls=self.num_parallel_calls)
+
+            datasets = [self.training_dataset_non_aug,
+                        self.training_dataset_aug]
+
+            # Define a dataset containing `[0, 1, 0, 1, 0, 1, ..., 0, 1]`.
+            choice_dataset = tf.data.Dataset.range(2).repeat(len(self.training_filenames))
+            self.training_dataset = tf.data.experimental.choose_from_datasets(datasets, choice_dataset)
+            print("Training on {} images ".format(len(self.training_filenames*2)))
+        else:
+            print("No data augmentation. Please set augment to True if you want to augment training dataset")
+            self.training_dataset = self.training_dataset.map((
+                    lambda x: tf.py_function(self.process_path, [x], self.output_shape)),
+                    num_parallel_calls=self.num_parallel_calls)
+            print("Training on {} images ".format(len(self.training_filenames)))
+
+
+        if self.enable_shuffle is True:
             if self.shuffle_and_repeat is False:
                 self.training_dataset = self.training_dataset.shuffle(self.training_data_shuffle_buffer_size)
             else:
                 self.logger.warning(
                     ('Can not enable just shuffling of dataset because shuffle and repeat enabled'))
 
-        if self.enable_just_repeat is True:
-            if self.shuffle_and_repeat is False:
-                self.training_dataset = self.training_dataset.repeat(
-                    count=self.data_repeat_count)
-            else:
-                self.logger.warning(
-                    ('Can not enable just repeat of dataset because shuffle and repeat enabled'))
-
-        if self.shuffle_and_repeat is True:
-            self.training_dataset = self.training_dataset.shuffle(self.training_data_shuffle_buffer_size)
-            self.training_dataset = self.training_dataset.repeat(count=self.data_repeat_count)
-
         if self.training_data_batch_size is not None:
             self.training_dataset = self.training_dataset.batch(self.training_data_batch_size)
 
-        if self.enable_data_prefetch is True:
+        if self.enable_data_prefetch:
             self.training_dataset = self.training_dataset.prefetch(self.data_prefetch_size)
 
     def build_validation_dataset(self):
         self.validation_dataset = tf.data.Dataset.from_tensor_slices(self.validation_filenames)
-        # self.validation_dataset = self.validation_dataset.map((lambda x: tf.py_function(self.process_path, [x], (tf.float32, tf.uint8))), num_parallel_calls=self.num_parallel_calls)
         self.validation_dataset = self.validation_dataset.map((lambda x: tf.py_function(self.process_path, [x], self.output_shape)), num_parallel_calls=self.num_parallel_calls)
 
         self.validation_dataset = self.validation_dataset.batch(self.training_data_batch_size)
-        self.validation_dataset = self.validation_dataset.prefetch(self.data_prefetch_size)
+        print("Validation on {} images ".format(len(self.validation_filenames)))
+
+        # self.validation_dataset = self.validation_dataset.prefetch(self.data_prefetch_size)
 
     def read_image(self, path_img):
         s3 = boto3.resource('s3')
@@ -122,15 +134,8 @@ class DataLoader:
                 train_img = np.transpose(src.read(), (1, 2, 0))
             else:
                 train_img = np.transpose(src.read(bands), (1, 2, 0))
-        # train_img = tf.convert_to_tensor(train_img / self.normalization_value, dtype=self.training_data_type)
-        # https://albumentations.ai/docs/examples/tensorflow-example/
-        # cast and normalize image
+        # Normalize image
         train_img = tf.image.convert_image_dtype(train_img, tf.float32)
-        # apply simple augmentations
-        train_img = tf.image.random_flip_left_right(train_img)
-        train_img = tf.image.random_flip_up_down(train_img)
-        train_img = tf.image.rot90(train_img)
-
         return train_img
 
     def get_label_from_csv(self, path_img):
@@ -149,23 +154,44 @@ class DataLoader:
         img = self.read_image(file_path)
         return img, label
 
+
+    def process_path_train_set_augment(self, file_path):
+        label = self.get_label_from_csv(file_path)
+        img = self.read_image(file_path)
+        # apply simple augmentations
+        if self.random_flip_up_down is True:
+            img = tf.image.random_flip_up_down(img)
+        if self.random_flip_left_right is True:
+            img = tf.image.random_flip_left_right(img)
+        if self.flip_left_right is True:
+            img = tf.image.flip_left_right(img)
+        if self.flip_up_down is True:
+            img = tf.image.flip_up_down(img)
+        if self.rot90 is True:
+            img = tf.image.rot90(img)
+        if self.transpose is True:
+            img = tf.image.transpose(img)
+
+        return img, label
+
 if __name__ == '__main__':
     gen = DataLoader(label_file_path_train="labels_test_v1.csv",
                         label_file_path_val="val_labels.csv",
                         bucket_name='canopy-production-ml',
                         data_extension_type='.tif',
                         training_data_shape=(100, 100, 18),
-                        shuffle_and_repeat=False,
-                        enable_just_shuffle=True,
-                        enable_just_repeat=False,
+                        augment=True,
+                        random_flip_up_down=True,
+                        random_flip_left_right=True,
+                        flip_left_right=True,
+                        flip_up_down=True,
+                        rot90=True,
+                        transpose=False,
+                        enable_shuffle=False,
                         training_data_shuffle_buffer_size=10,
-                        data_repeat_count=None,
                         training_data_batch_size=20,
-                        normalization_value=255.0,  #normalization
                         training_data_type=tf.float32,
                         label_data_type=tf.uint8,
-                        enable_data_prefetch=False,
-                        data_prefetch_size=tf.data.experimental.AUTOTUNE,
                         num_parallel_calls=int(2))
 
     no_of_val_imgs = len(gen.validation_filenames)
