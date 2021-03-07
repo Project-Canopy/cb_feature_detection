379e78f110b9690ccbd3d5883355c568c1ccab44
Margaux Masson-Forsythe <margaux.masson21@gmail.com>
2021-03-07 13:43:37 -0800

    Added wandb, new metrics and focal loss

--- model-development/data_loader.py
+++ model-development/data_loader.py
@@ -15,6 +15,7 @@ import boto3
 import io
 import json
 
+
 class DataLoader:
     def __init__(self, label_file_path_train="labels_test_v1.csv",
                  label_file_path_val="labels_val.csv",
@@ -39,7 +40,7 @@ class DataLoader:
                  enable_data_prefetch=False,
                  data_prefetch_size=tf.data.experimental.AUTOTUNE,
                  num_parallel_calls=2 * multiprocessing.cpu_count(),
-                 output_shape=(tf.float32, tf.uint8)):
+                 output_shape=(tf.float32, tf.float32)):
 
         self.s3 = boto3.resource('s3')
         self.bucket_name = bucket_name
@@ -62,7 +63,7 @@ class DataLoader:
 
         self.labels_file_val = pd.read_csv(self.label_file_path_val)
         self.validation_filenames = self.labels_file_val.paths.to_list()
-        
+
         self.bands = bands
 
         self.augment = augment
@@ -83,25 +84,24 @@ class DataLoader:
         self.training_data_shape = training_data_shape
         self.training_data_shuffle_buffer_size = \
             training_data_shuffle_buffer_size \
-            if training_data_shuffle_buffer_size is not None \
-            else len(self.training_filenames) // training_data_batch_size
+                if training_data_shuffle_buffer_size is not None \
+                else len(self.training_filenames) // training_data_batch_size
 
         self.training_data_batch_size = training_data_batch_size
         self.training_data_type = training_data_type
         self.label_data_type = label_data_type
 
+        self.class_weight_train = self.generate_class_weight(self.label_file_path_train)
+        self.class_weight_val = self.generate_class_weight(self.label_file_path_val)
+
         self.build_training_dataset()
         self.build_validation_dataset()
 
-        self.class_weight = self.generate_class_weight()
-
-
-    def read_s3_obj(self,s3_key):
+    def read_s3_obj(self, s3_key):
         s3 = self.s3
         obj = s3.Object(self.bucket_name, s3_key)
         obj_bytes = io.BytesIO(obj.get()['Body'].read())
         return obj_bytes
-
 
     def build_training_dataset(self):
         self.training_dataset = tf.data.Dataset.from_tensor_slices(self.training_filenames)
@@ -121,12 +121,12 @@ class DataLoader:
             # Define a dataset containing `[0, 1, 0, 1, 0, 1, ..., 0, 1]`.
             choice_dataset = tf.data.Dataset.range(2).repeat(len(self.training_filenames))
             self.training_dataset = tf.data.experimental.choose_from_datasets(datasets, choice_dataset)
-            print("Training on {} images ".format(len(self.training_filenames*2)))
+            print("Training on {} images ".format(len(self.training_filenames * 2)))
         else:
             print("No data augmentation. Please set augment to True if you want to augment training dataset")
             self.training_dataset = self.training_dataset.map((
-                    lambda x: tf.py_function(self.process_path, [x], self.output_shape)),
-                    num_parallel_calls=self.num_parallel_calls)
+                lambda x: tf.py_function(self.process_path, [x], self.output_shape)),
+                num_parallel_calls=self.num_parallel_calls)
             print("Training on {} images ".format(len(self.training_filenames)))
 
         if self.enable_shuffle is True:
@@ -140,7 +140,9 @@ class DataLoader:
 
     def build_validation_dataset(self):
         self.validation_dataset = tf.data.Dataset.from_tensor_slices(list(self.validation_filenames))
-        self.validation_dataset = self.validation_dataset.map((lambda x: tf.py_function(self.process_path, [x], self.output_shape)), num_parallel_calls=self.num_parallel_calls)
+        self.validation_dataset = self.validation_dataset.map(
+            (lambda x: tf.py_function(self.process_path, [x], self.output_shape)),
+            num_parallel_calls=self.num_parallel_calls)
 
         self.validation_dataset = self.validation_dataset.batch(self.training_data_batch_size)
         print("Validation on {} images ".format(len(self.validation_filenames)))
@@ -171,11 +173,16 @@ class DataLoader:
             label = self.labels_file_val.drop('paths', 1).iloc[int(id)].to_list()
         return label
 
+    # def get_weights(self, class_weight):
+    #     weights = np.zeros(10)
+    #     for id in range(len(class_weight)):
+    #         weights[list(class_weight.keys())[id]] = list(class_weight.values())[id]
+    #     return weights
+
     def process_path(self, file_path):
         label = self.get_label_from_csv(file_path)
         img = self.read_image(file_path)
         return img, label
-
 
     def process_path_train_set_augment(self, file_path):
         label = self.get_label_from_csv(file_path)
@@ -196,11 +203,11 @@ class DataLoader:
             img = tf.image.transpose(img)
 
         return img, label
-    
-    def generate_class_weight(self):
+
+    def generate_class_weight(self, file_path):
 
         # generate class_weights dict to be used for class_weight attribute in model
-    
+
         df = self.labels_file_train
         if not self.s3_file_paths:
             data = json.load(open(self.label_mapping_path))
@@ -215,37 +222,40 @@ class DataLoader:
                 labels[column] = col_count
             except:
                 no_label_class.append(column)
-        
-        print(f"Your training file is missing positive labels for classes {no_label_class}")
+
+        print(f"The file {file_path} is missing positive labels for classes {no_label_class}")
 
         labels_sum = sum(labels.values())
         keys_len = len(labels.keys())
 
         class_weight = {}
         for label_name in labels.keys():
-            class_weight[int(label_name)] = (1 / labels[label_name]) * (labels_sum)/keys_len
+            class_weight[int(label_name)] = (1 / labels[label_name]) * (labels_sum) / keys_len
 
         return class_weight
 
+
 if __name__ == '__main__':
     gen = DataLoader(label_file_path_train="labels_test_v1.csv",
-                        label_file_path_val="val_labels.csv",
-                        bucket_name='canopy-production-ml',
-                        data_extension_type='.tif',
-                        training_data_shape=(100, 100, 18),
-                        augment=True,
-                        random_flip_up_down=True,
-                        random_flip_left_right=True,
-                        flip_left_right=True,
-                        flip_up_down=True,
-                        rot90=True,
-                        transpose=False,
-                        enable_shuffle=False,
-                        training_data_shuffle_buffer_size=10,
-                        training_data_batch_size=20,
-                        training_data_type=tf.float32,
-                        label_data_type=tf.uint8,
-                        num_parallel_calls=int(2))
+                     label_file_path_val="val_labels.csv",
+                     bucket_name='canopy-production-ml',
+                     s3_file_paths=False,
+                     data_extension_type='.tif',
+                     training_data_shape=(100, 100, 18),
+                     augment=True,
+                     random_flip_up_down=True,
+                     random_flip_left_right=True,
+                     flip_left_right=True,
+                     flip_up_down=True,
+                     rot90=True,
+                     transpose=False,
+                     enable_shuffle=True,
+                     training_data_shuffle_buffer_size=10,
+                     training_data_batch_size=20,
+                     training_data_type=tf.float32,
+                     label_data_type=tf.uint8,
+                     num_parallel_calls=int(2))
+
 
     def Simple_CNN(numclasses, input_shape):
         model = Sequential([
@@ -262,6 +272,7 @@ if __name__ == '__main__':
         ])
         return model
 
+
     def Resnet50(numclasses, input_shape):
         model = Sequential()
         model.add(keras.applications.ResNet50(include_top=False, pooling='avg', weights=None, input_shape=input_shape))
@@ -275,12 +286,13 @@ if __name__ == '__main__':
         model.layers[0].trainable = True
         return model
 
+
     model = Resnet50(10, input_shape=(100, 100, 18))
     # model = Simple_CNN(10, input_shape=(100, 100, 18))
     callbacks_list = []
 
     model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              optimizer=keras.optimizers.Adam())
+                  optimizer=keras.optimizers.Adam())
 
     epochs = 10
     history = model.fit(gen.training_dataset, validation_data=gen.validation_dataset, epochs=epochs)
