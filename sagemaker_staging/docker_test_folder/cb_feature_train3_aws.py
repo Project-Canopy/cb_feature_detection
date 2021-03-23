4edea43fb5ca650f40010654507827280dca5542
Zhenya <zwarshavsky@gmail.com>
2021-03-23 20:53:55 +0000

    updates for fsx / s3 path copy to ml/output/data support

--- sagemaker_staging/docker_test_folder/cb_feature_train3_aws.py
+++ sagemaker_staging/docker_test_folder/cb_feature_train3_aws.py
@@ -111,202 +111,204 @@ class SaveCheckpoints(keras.callbacks.Callback):
         
 
 if __name__ == '__main__':
-
-
-    local_test = False
     
-    check_gpu()
+    print(os.command("ls /opt/ml/data/fsx/"))
+
+
+#     local_test = False
     
-    args, _ = parse_args()
+#     check_gpu()
     
-    wandb_key = args.wandb_key
-    os.environ["WANDB_API_KEY"]=wandb_key
-    wandb.init(project='project_canopy', tensorboard=True)
-    config = wandb.config
+#     args, _ = parse_args()
     
-    job_name = args.job_name
-    data_dir = args.data
-    lcl_chkpt_dir = args.model_dir + "/" + "checkpoints"
-    s3_chkpt_base_dir = args.s3_chkpt_dir
-    epochs = args.epochs
-    batch_size=args.batch_size
-    bands = [int(band) for band in args.bands.split(',')]
-    starting_checkpoint = args.starting_checkpoint
-    print('Starting checkpoint:', starting_checkpoint)
+#     wandb_key = args.wandb_key
+#     os.environ["WANDB_API_KEY"]=wandb_key
+#     wandb.init(project='project_canopy', tensorboard=True)
+#     config = wandb.config
     
-    if not os.path.exists(lcl_chkpt_dir):
-        os.mkdir(lcl_chkpt_dir)
+#     job_name = args.job_name
+#     data_dir = args.data
+#     lcl_chkpt_dir = args.model_dir + "/" + "checkpoints"
+#     s3_chkpt_base_dir = args.s3_chkpt_dir
+#     epochs = args.epochs
+#     batch_size=args.batch_size
+#     bands = [int(band) for band in args.bands.split(',')]
+#     starting_checkpoint = args.starting_checkpoint
+#     print('Starting checkpoint:', starting_checkpoint)
+    
+#     if not os.path.exists(lcl_chkpt_dir):
+#         os.mkdir(lcl_chkpt_dir)
 
-    files = ["labels_train.csv","labels_val.csv","labels.json"]
+#     files = ["labels_train.csv","labels_val.csv","labels.json"]
 
 
-    if local_test:
+#     if local_test:
 
-        base_path = "/Users/purgatorid/Documents/GitHub/Project Canopy/cb_feature_detection/sagemaker_staging/"
+#         base_path = "/Users/purgatorid/Documents/GitHub/Project Canopy/cb_feature_detection/sagemaker_staging/"
 
-        label_file_path_train = base_path + files[0]
-        label_file_path_val = base_path + files[1]
-        label_mapping_path = base_path + files[2]
-        lcl_chkpt_dir = "./checkpoints"
+#         label_file_path_train = base_path + files[0]
+#         label_file_path_val = base_path + files[1]
+#         label_mapping_path = base_path + files[2]
+#         lcl_chkpt_dir = "./checkpoints"
 
-    else:
+#     else:
 
-        label_file_path_train = os.path.join(data_dir, files[0])
-        label_file_path_val = os.path.join(data_dir,files[1])
-        label_mapping_path = os.path.join(data_dir,files[2])
+#         label_file_path_train = os.path.join(data_dir, files[0])
+#         label_file_path_val = os.path.join(data_dir,files[1])
+#         label_mapping_path = os.path.join(data_dir,files[2])
 
     
-    # Variables definition
-    config.batch_size = batch_size
-    config.learning_rate = 0.001
-    config.label_file_path_train=label_file_path_train # labels_1_4_train_v2
-    config.label_file_path_val=label_file_path_val
-    config.loss = SigmoidFocalCrossEntropy() # tf.keras.losses.BinaryCrossentropy(from_logits=False)
-    config.optimizer = keras.optimizers.Adam(config.learning_rate)
-    config.input_shape = (100, 100, len(bands))
-    config.numclasses=10
+#     # Variables definition
+#     config.batch_size = batch_size
+#     config.learning_rate = 0.001
+#     config.label_file_path_train=label_file_path_train # labels_1_4_train_v2
+#     config.label_file_path_val=label_file_path_val
+#     config.loss = SigmoidFocalCrossEntropy() # tf.keras.losses.BinaryCrossentropy(from_logits=False)
+#     config.optimizer = keras.optimizers.Adam(config.learning_rate)
+#     config.input_shape = (100, 100, len(bands))
+#     config.numclasses=10
     
-    config.random_flip_up_down=False
-    config.random_flip_left_right=False
-    config.transpose=False
-    config.enable_shuffle=True
+#     config.random_flip_up_down=False
+#     config.random_flip_left_right=False
+#     config.transpose=False
+#     config.enable_shuffle=True
     
-#     config.augment = augment
-#     config.flip_left_right = flip_left_right
-#     config.flip_up_down = flip_up_down
-#     config.rot90 = rot90
+# #     config.augment = augment
+# #     config.flip_left_right = flip_left_right
+# #     config.flip_up_down = flip_up_down
+# #     config.rot90 = rot90
 
-    print(f"batch size {config.batch_size}, learning_rate {config.learning_rate}, augment {config.augment}")
+#     print(f"batch size {config.batch_size}, learning_rate {config.learning_rate}, augment {config.augment}")
     
 
-    gen = DataLoader(label_file_path_train=config.label_file_path_train, # test labels_test_v1 TODO use s3 paths
-                    label_file_path_val=config.label_file_path_val, # or val_all
-                    label_mapping_path=label_mapping_path, 
-                    bucket_name='canopy-production-ml',
-                    data_extension_type='.tif',
-                    #training_data_shape=config.input_shape,
-                    augment=config.augment,
-                    s3_file_paths=False,
-                    flip_left_right=config.flip_left_right,
-                    flip_up_down=config.flip_up_down,
-                    rot90=config.rot90,
-                    transpose=config.transpose,
-                    enable_shuffle=config.enable_shuffle,
-                    training_data_batch_size=config.batch_size,
-                    enable_data_prefetch=True,
-                    bands=bands
-                    )
+#     gen = DataLoader(label_file_path_train=config.label_file_path_train, # test labels_test_v1 TODO use s3 paths
+#                     label_file_path_val=config.label_file_path_val, # or val_all
+#                     label_mapping_path=label_mapping_path, 
+#                     bucket_name='canopy-production-ml',
+#                     data_extension_type='.tif',
+#                     #training_data_shape=config.input_shape,
+#                     augment=config.augment,
+#                     s3_file_paths=False,
+#                     flip_left_right=config.flip_left_right,
+#                     flip_up_down=config.flip_up_down,
+#                     rot90=config.rot90,
+#                     transpose=config.transpose,
+#                     enable_shuffle=config.enable_shuffle,
+#                     training_data_batch_size=config.batch_size,
+#                     enable_data_prefetch=True,
+#                     bands=bands
+#                     )
     
-    def define_model(numclasses,input_shape,starting_checkpoint=None,lcl_chkpt_dir=None):
-        # parameters for CNN
-        input_tensor = Input(shape=input_shape)
+#     def define_model(numclasses,input_shape,starting_checkpoint=None,lcl_chkpt_dir=None):
+#         # parameters for CNN
+#         input_tensor = Input(shape=input_shape)
 
-        # introduce a additional layer to get from bands to 3 input channels
-        input_tensor = Conv2D(3, (1, 1))(input_tensor)
+#         # introduce a additional layer to get from bands to 3 input channels
+#         input_tensor = Conv2D(3, (1, 1))(input_tensor)
 
-        base_model_resnet50 = keras.applications.ResNet50(include_top=False,
-                                  weights='imagenet',
-                                  input_shape=(100, 100, 3))
-        base_model = keras.applications.ResNet50(include_top=False,
-                         weights=None,
-                         input_tensor=input_tensor)
+#         base_model_resnet50 = keras.applications.ResNet50(include_top=False,
+#                                   weights='imagenet',
+#                                   input_shape=(100, 100, 3))
+#         base_model = keras.applications.ResNet50(include_top=False,
+#                          weights=None,
+#                          input_tensor=input_tensor)
 
-        for i, layer in enumerate(base_model_resnet50.layers):
-            # we must skip input layer, which has no weights
-            if i == 0:
-                continue
-            base_model.layers[i+1].set_weights(layer.get_weights())
+#         for i, layer in enumerate(base_model_resnet50.layers):
+#             # we must skip input layer, which has no weights
+#             if i == 0:
+#                 continue
+#             base_model.layers[i+1].set_weights(layer.get_weights())
 
-        # add a global spatial average pooling layer
-        top_model = base_model.output
-        top_model = GlobalAveragePooling2D()(top_model)
+#         # add a global spatial average pooling layer
+#         top_model = base_model.output
+#         top_model = GlobalAveragePooling2D()(top_model)
 
-        # let's add a fully-connected layer
-        top_model = Dense(2048, activation='relu')(top_model)
-        top_model = Dense(2048, activation='relu')(top_model)
-        # and a logistic layer
-        predictions = Dense(numclasses, activation='sigmoid')(top_model)
+#         # let's add a fully-connected layer
+#         top_model = Dense(2048, activation='relu')(top_model)
+#         top_model = Dense(2048, activation='relu')(top_model)
+#         # and a logistic layer
+#         predictions = Dense(numclasses, activation='sigmoid')(top_model)
 
-        # this is the model we will train
-        model = Model(inputs=base_model.input, outputs=predictions)
-    #     model = model.layers[-1].bias.assign([0.0]) # WIP getting an error ValueError: Cannot assign to variable dense_8/bias:0 due to variable shape (10,) and value shape (1,) are incompatible
+#         # this is the model we will train
+#         model = Model(inputs=base_model.input, outputs=predictions)
+#     #     model = model.layers[-1].bias.assign([0.0]) # WIP getting an error ValueError: Cannot assign to variable dense_8/bias:0 due to variable shape (10,) and value shape (1,) are incompatible
 
-    #     model.summary()
-        last_chkpt_path = lcl_chkpt_dir + 'last_chkpt.h5'
-        if os.path.exists(last_chkpt_path):
-            print('New spot instance; loading previous checkpoint')
-            model.load_weights(last_chkpt_path)
-        elif starting_checkpoint:
-            print('No previous checkpoint found in opt/ml/checkpoints directory; loading checkpoint from', starting_checkpoint)
-            s3 = boto3.client('s3')
-            chkpt_name = lcl_chkpt_dir + '/' + 'start_chkpt.h5'
-            s3.download_file('canopy-production-ml-output', starting_checkpoint, chkpt_name)
-            model.load_weights(chkpt_name)
-        else:
-            print('No previous checkpoint found in opt/ml/checkpoints directory; start training from scratch')
+#     #     model.summary()
+#         last_chkpt_path = lcl_chkpt_dir + 'last_chkpt.h5'
+#         if os.path.exists(last_chkpt_path):
+#             print('New spot instance; loading previous checkpoint')
+#             model.load_weights(last_chkpt_path)
+#         elif starting_checkpoint:
+#             print('No previous checkpoint found in opt/ml/checkpoints directory; loading checkpoint from', starting_checkpoint)
+#             s3 = boto3.client('s3')
+#             chkpt_name = lcl_chkpt_dir + '/' + 'start_chkpt.h5'
+#             s3.download_file('canopy-production-ml-output', starting_checkpoint, chkpt_name)
+#             model.load_weights(chkpt_name)
+#         else:
+#             print('No previous checkpoint found in opt/ml/checkpoints directory; start training from scratch')
     
-        return model
+#         return model
     
-#     random_id = random.randint(1,10001)
+# #     random_id = random.randint(1,10001)
 
-    s3_chkpt_dir = s3_chkpt_base_dir + "/" + job_name   
-    base_name_checkpoint = "model_resnet"
-    save_checkpoint_s3 = SaveCheckpoints(base_name_checkpoint,lcl_chkpt_dir,s3_chkpt_dir)
+#     s3_chkpt_dir = s3_chkpt_base_dir + "/" + job_name   
+#     base_name_checkpoint = "model_resnet"
+#     save_checkpoint_s3 = SaveCheckpoints(base_name_checkpoint,lcl_chkpt_dir,s3_chkpt_dir)
 
 
-    # model_checkpoint_callback_loss = tf.keras.callbacks.ModelCheckpoint(
-    #   filepath= f'checkpoint/checkpoint_loss_{random_id}.h5',
-    #   format='h5',
-    #   verbose=1,
-    #   save_weights_only=True,
-    #   monitor='val_loss',
-    #   mode='min',
-    #   save_best_only=True)
+#     # model_checkpoint_callback_loss = tf.keras.callbacks.ModelCheckpoint(
+#     #   filepath= f'checkpoint/checkpoint_loss_{random_id}.h5',
+#     #   format='h5',
+#     #   verbose=1,
+#     #   save_weights_only=True,
+#     #   monitor='val_loss',
+#     #   mode='min',
+#     #   save_best_only=True)
 
-    # model_checkpoint_callback_recall = tf.keras.callbacks.ModelCheckpoint(
-    #   filepath= f'checkpoint/checkpoint_recall_{random_id}.h5',
-    #   format='h5',
-    #   verbose=1,
-    #   save_weights_only=True,
-    #   monitor='val_recall',
-    #   mode='max',
-    #   save_best_only=True)
+#     # model_checkpoint_callback_recall = tf.keras.callbacks.ModelCheckpoint(
+#     #   filepath= f'checkpoint/checkpoint_recall_{random_id}.h5',
+#     #   format='h5',
+#     #   verbose=1,
+#     #   save_weights_only=True,
+#     #   monitor='val_recall',
+#     #   mode='max',
+#     #   save_best_only=True)
 
-    # model_checkpoint_callback_precision = tf.keras.callbacks.ModelCheckpoint(
-    #   filepath= f'checkpoint/checkpoint_precision_{random_id}.h5',
-    #   format='h5',
-    #   verbose=1,
-    #   save_weights_only=True,
-    #   monitor='val_precision',
-    #   mode='max',
-    #   save_best_only=True)
+#     # model_checkpoint_callback_precision = tf.keras.callbacks.ModelCheckpoint(
+#     #   filepath= f'checkpoint/checkpoint_precision_{random_id}.h5',
+#     #   format='h5',
+#     #   verbose=1,
+#     #   save_weights_only=True,
+#     #   monitor='val_precision',
+#     #   mode='max',
+#     #   save_best_only=True)
 
-    # reducelronplateau = tf.keras.callbacks.ReduceLROnPlateau(
-    #   monitor='val_loss', factor=0.1, patience=5, verbose=1,
-    #   mode='min', min_lr=0.000001)
+#     # reducelronplateau = tf.keras.callbacks.ReduceLROnPlateau(
+#     #   monitor='val_loss', factor=0.1, patience=5, verbose=1,
+#     #   mode='min', min_lr=0.000001)
 
-    early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_precision', mode='max', patience=20, verbose=1)
+#     early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_precision', mode='max', patience=20, verbose=1)
 
-    wandb_callback = WandbCallback()
-    callbacks_list = [save_checkpoint_s3,early_stop, wandb_callback] #TODO re add reducelronplateau
+#     wandb_callback = WandbCallback()
+#     callbacks_list = [save_checkpoint_s3,early_stop, wandb_callback] #TODO re add reducelronplateau
 
-    model = define_model(config.numclasses, config.input_shape, starting_checkpoint, lcl_chkpt_dir)
+#     model = define_model(config.numclasses, config.input_shape, starting_checkpoint, lcl_chkpt_dir)
 
-    # loss=tf.keras.losses.BinaryCrossentropy(from_logits=False), # Computes the cross-entropy loss between true labels and predicted labels.
-    # Focal loss instead of class weights: https://www.dlology.com/blog/multi-class-classification-with-focal-loss-for-imbalanced-datasets/
-    model.compile(loss=SigmoidFocalCrossEntropy(), # https://www.tensorflow.org/addons/api_docs/python/tfa/losses/SigmoidFocalCrossEntropy
-                  optimizer=keras.optimizers.Adam(config.learning_rate),
-                  metrics=[tf.metrics.BinaryAccuracy(name='accuracy'), 
-                           tf.keras.metrics.Precision(name='precision'), # Computes the precision of the predictions with respect to the labels.
-                           tf.keras.metrics.Recall(name='recall'), # Computes the recall of the predictions with respect to the labels.
-                           F1Score(num_classes=10, name="f1_score") # https://www.tensorflow.org/addons/api_docs/python/tfa/metrics/F1Score
-                           ]
-    #               sample_weight_mode="temporal" # This argument is not supported when x is a dataset or a dataset iterator, instead pass sample weights as the third element of x.
-                 )
+#     # loss=tf.keras.losses.BinaryCrossentropy(from_logits=False), # Computes the cross-entropy loss between true labels and predicted labels.
+#     # Focal loss instead of class weights: https://www.dlology.com/blog/multi-class-classification-with-focal-loss-for-imbalanced-datasets/
+#     model.compile(loss=SigmoidFocalCrossEntropy(), # https://www.tensorflow.org/addons/api_docs/python/tfa/losses/SigmoidFocalCrossEntropy
+#                   optimizer=keras.optimizers.Adam(config.learning_rate),
+#                   metrics=[tf.metrics.BinaryAccuracy(name='accuracy'), 
+#                            tf.keras.metrics.Precision(name='precision'), # Computes the precision of the predictions with respect to the labels.
+#                            tf.keras.metrics.Recall(name='recall'), # Computes the recall of the predictions with respect to the labels.
+#                            F1Score(num_classes=10, name="f1_score") # https://www.tensorflow.org/addons/api_docs/python/tfa/metrics/F1Score
+#                            ]
+#     #               sample_weight_mode="temporal" # This argument is not supported when x is a dataset or a dataset iterator, instead pass sample weights as the third element of x.
+#                  )
     
-    epochs = epochs
-    history = model.fit(gen.training_dataset, validation_data=gen.validation_dataset, 
-                        epochs=epochs, 
-                        callbacks=callbacks_list,
-                        shuffle=True # whether to shuffle the training data before each epoch
-                       ) 
+#     epochs = epochs
+#     history = model.fit(gen.training_dataset, validation_data=gen.validation_dataset, 
+#                         epochs=epochs, 
+#                         callbacks=callbacks_list,
+#                         shuffle=True # whether to shuffle the training data before each epoch
+#                        ) 
